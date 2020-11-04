af8838435264a3caef9a8a690fa5e62fc5cd60e5
HassanTSF <hassantsf.dev@gmail.com>
2020-11-04 15:35:12 +0330

    Add Enum classes

---
+++ src/main/java/ir/ac/kntu/SendMethod.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu;
+
+public enum SendMethod {
+  Air_MAIL,
+  Ground_MAIL,
+  SEA_MAIL
+}
---
+++ src/main/java/ir/ac/kntu/SendingStatus.java
@@ -0,0 +1,7 @@
+package ir.ac.kntu;
+
+public enum SendingStatus {
+  STORED,
+  POSTED,
+  RECEIVED
+}
