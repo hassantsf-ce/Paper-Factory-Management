902c27224a5ea59d3bdc75f0e0a727a82ffc3f64
HassanTSF <hassantsf.dev@gmail.com>
2020-11-06 13:14:51 +0330

    add city validation

--- src/main/java/ir/ac/kntu/controller/CityController.java
+++ src/main/java/ir/ac/kntu/controller/CityController.java
@@ -2,26 +2,28 @@ package ir.ac.kntu.controller;
 
 import ir.ac.kntu.exceptions.CanNotInstantiateException;
 import ir.ac.kntu.model.City;
+import ir.ac.kntu.util.CityValidation;
 import ir.ac.kntu.util.PrimaryValidation;
 import ir.ac.kntu.util.UserInput;
 
 public class CityController implements Controller<City> {
   private UserInput input;
-  private PrimaryValidation validator;
+  private CityValidation validator;
 
   public CityController() {
     this.input = new UserInput();
-    validator = new PrimaryValidation();
+    validator = new CityValidation();
   }
 
   @Override
   public City create() throws CanNotInstantiateException {
     System.out.println("-- Create New City --");
-    String cityName = input.getString("name");
-    String cityState = input.getString("state name");
+    String cityName = input.getLine("name");
+    String cityState = input.getLine("state name");
     try {
       validator.validateString(cityName);
       validator.validateString(cityState);
+      validator.validateCityNameAndStateValidation(cityName, cityState);
       return new City(cityName, cityState);
     } catch (Exception e) {
       System.out.println(e.getMessage());
---
+++ src/main/java/ir/ac/kntu/util/CityValidation.java
@@ -0,0 +1,18 @@
+package ir.ac.kntu.util;
+
+import ir.ac.kntu.exceptions.EmptyStringException;
+import ir.ac.kntu.exceptions.NullValueException;
+import ir.ac.kntu.exceptions.ShortLengthStringException;
+
+public class CityValidation extends PrimaryValidation {
+
+  public void validateCityNameAndStateValidation(String name, String state) throws ShortLengthStringException, EmptyStringException, NullValueException {
+    this.validateString(name);
+    this.validateString(state);
+
+    final int LENGTH_LIMIT = 3;
+    if (name.length() < LENGTH_LIMIT || state.length() < LENGTH_LIMIT) {
+      throw new ShortLengthStringException();
+    }
+  }
+}
